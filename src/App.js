900f819977a3ccf1cb7a48325a671b4f25779b44
duyv <duyv0304@gmail.com>
2022-05-07 14:09:34 +0700

    update week-5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navigation from "./navigation";
 import RootRouter from "./router";
